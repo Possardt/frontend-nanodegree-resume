d9206caf28e2dfc4e3aa82261709d760458c7a91
Ryan Possardt <rpossardt@gmail.com>
2017-02-26 20:55:18 -0500

    committing changes for FEND class

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,7 +14,7 @@ This is empty on purpose! Your code to build the resume will go here.
  		twitter : '@Ryan_Possardt',
  		location : 'CT, USA'
  	},
- 	skills : ['sports', 'programming', 'hiking'];
+ 	skills : ['sports', 'programming', 'hiking']
 
  };
 
@@ -25,6 +25,12 @@ This is empty on purpose! Your code to build the resume will go here.
  			title : 'Shop Technician',
  			dates : '01/2014 - 05/2016',
  			description : 'fixed college kids computers for free'
+ 		},
+ 		{
+ 			employer : 'Great Lakes',
+ 			title : 'Developer',
+ 			dates : '05/2016 - Present',
+ 			description : 'Work on IDR internal application'
  		}
  	]
  };
@@ -32,7 +38,7 @@ This is empty on purpose! Your code to build the resume will go here.
  var projects = {
  	projects : [
  		{
- 			title : 'Pong'.
+ 			title : 'Pong',
  			dates : '01/2016 - 05/2016',
  			description : 'Remade Pong in Java using Swing for CSE 2102',
  			images : [
@@ -50,9 +56,9 @@ This is empty on purpose! Your code to build the resume will go here.
  			location : 'Storrs, CT',
  			degreeDates : '2012 - 2016',
  			url : 'uconn.edu',
- 			majors : ['MIS']
+ 			majors : ['MIS', 'CS']
  		}
- 	]
+ 	],
  	onlineCourses : [
  		{
  			title : 'Front-End Web Developer',
@@ -67,3 +73,74 @@ This is empty on purpose! Your code to build the resume will go here.
 
  $('#header').prepend(formattedName);
  $('#header').prepend(formattedRole);
+
+bio.display = function () {
+	var bioPic = HTMLbioPic.replace('%data%', this.biopic);
+	var welcomeMsg = HTMLwelcomeMsg.replace('%data%', this.welcomeMessage);
+
+	$('#header').append(bioPic).append(welcomeMsg);
+
+	var mobileContact = HTMLmobile.replace('%data%', this.contacts.mobile);
+	var emailContact = HTMLemail.replace('%data%', this.contacts.email);
+	var twitterContact = HTMLtwitter.replace('%data%', this.contacts.twitter);
+	var githubContact = HTMLgithub.replace('%data%', this.contacts.github);
+	var locationContact = HTMLlocation.replace('%data%',this.contacts.location);
+	$('#topContacts').append(mobileContact).append(emailContact).append(twitterContact)
+							.append(githubContact).append(locationContact);
+
+};
+
+education.display = function () {
+	$('#education').append(HTMLschoolStart);
+	for(var i = 0; i < this.schools.length; i++){
+		var schoolName = HTMLschoolName.replace('%data%', this.schools[i].name) + 
+							HTMLschoolDegree.replace('%data%', 'Bachelor of Science');
+		var schoolLocation = HTMLschoolLocation.replace('%data%', this.schools[i].location);
+		var schoolDates = HTMLschoolDates.replace('%data%', this.schools[i].degreeDates);
+		var majors = this.schools[i].majors.join(', ');
+		var schoolMajor = HTMLschoolMajor.replace('%data%', this.schools[i].majors);
+		$('.education-entry:last').append(schoolName).append(schoolLocation)
+									.append(schoolDates).append(schoolMajor);
+	}
+	$('.education-entry:last').append(HTMLonlineClasses);
+	for(var i = 0; i < this.onlineCourses.length; i++){
+		var courseTitle = HTMLonlineTitle.replace('%data%', this.onlineCourses[i].title) +
+							HTMLonlineSchool.replace('%data%',this.onlineCourses[i].school);
+		var courseDates = HTMLonlineDates.replace('%data%', this.onlineCourses[i].dates);
+		var courseURL = HTMLonlineURL.replace('%data%', this.onlineCourses[i].url);
+		$('.education-entry:last').append(courseTitle).append(courseDates).append(courseURL);
+	}
+};
+
+work.display = function(){
+	$('#workExperience').append(HTMLworkStart);
+	for(var i = 0; i < this.jobs.length; i++){
+		var jobEmployer = HTMLworkEmployer.replace('%data%', this.jobs[i].employer);
+		var jobName = HTMLworkTitle.replace('%data%', this.jobs[i].title);
+		var jobDates = HTMLworkDates.replace('%data%', this.jobs[i].dates);
+		var jobDescription = HTMLworkDescription.replace('%data%', this.jobs[i].description);
+
+		$('.work-entry:last').append(
+			jobEmployer + jobName	
+		).append(jobDates).append(jobDescription);
+	}
+};
+
+projects.display = function () {
+	$('#projects').append(HTMLprojectStart);
+	for(var i = 0; i < this.projects.length; i++){
+		var projectTitle = HTMLprojectTitle.replace('%data%', this.projects[i].title);
+		var projectDates = HTMLprojectDates.replace('%data%', this.projects[i].dates);
+		var projectDescription = HTMLprojectDescription.replace('%data%', this.projects[i].description);
+		var projectImage = HTMLprojectImage.replace('%data%', this.projects[i].images[0]);
+		$('.project-entry:last').append(projectTitle).append(projectDates).append(projectDescription)
+							.append(projectImage);
+	}
+};
+
+work.display();
+projects.display();
+bio.display();
+education.display();
+
+$('#mapDiv').append(googleMap);
